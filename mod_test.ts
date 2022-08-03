e425923a833b34f71faade031750da5c042d88ee
迷渡 <justjavac@gmail.com>
2022-08-03 21:49:46 +0800

    upgrade std@0.150.0 (#3)

--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.123.0/testing/asserts.ts";
+} from "https://deno.land/std@0.150.0/testing/asserts.ts";
 
 import * as ieee754 from "./mod.ts";
 
